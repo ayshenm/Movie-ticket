14a3299645d610ee801e18d793c1cec3d3707872
ayshenm <84517249+ayshenm@users.noreply.github.com>
2021-08-04 13:06:32 +0400

    saveToLocalStorage
    04.08.2021

--- move ticket/script.js
+++ move ticket/script.js
@@ -2,16 +2,49 @@ const container = document.querySelector('.container');
 const count = document.querySelector('#count');
 const amount = document.querySelector('#amount');
 const select = document.querySelector('#movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)');
 
 
 container.addEventListener('click',function(e){
     if(e.target.classList.contains('seat')&& !e.target.classList.contains('reserved')){
         e.target.classList.toggle('selected');
-
-        let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-        count.innerText = selectedSeatCount;
-        
+        calculateTotal();    
 
     } 
 
 });
+
+select.addEventListener('change',function(e){
+    calculateTotal();
+
+});
+
+function calculateTotal(){
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr =[];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+    let selectedSeatIndexs = selectedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+
+    let selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocalStorage(selectedSeatIndexs);
+}
+
+function saveToLocalStorage(index){
+    localStorage.setItem('selectedSeats', JSON.stringify(index));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
+}
